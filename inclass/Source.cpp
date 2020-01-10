057e2d6cf311ea3bfe9a68c4e121b0a74f8e74e4
donteamie@gmail.com <donteamie@gmail.com>
2020-01-10 14:56:02 -0700

    Add project files.

---
+++ inclass/Source.cpp
@@ -0,0 +1,19 @@
+//Marcel Amie Jr.
+#include <iostream>
+#include <string>
+#include<cmath>
+using namespace std;
+int main() {
+	/*float kilogram, pound;
+	cout << "Enter pound";
+	cin >> pound;
+	kilogram = pound * .454;
+	cout << pound << " pound = " << kilogram << " kg";
+}*/
+
+
+	int s;
+	cout << "please enter";
+	cin >> s;
+	cout << "the area is" << (((3 * sqrt(3)) / (2)) * (pow(s, 2.0)));
+}
